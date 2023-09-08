8b14a82b6ac03eca686e22eda6f77f73b7138278
Sean L <slava_be@hotmail.com>
2023-09-08 21:48:56 +0200

    Added questions + game + picocss

---
+++ src/routes/+page.js
@@ -0,0 +1,25 @@
+export function load() {
+    return {
+        questions: [
+            "Wie heeft de grootste schoenen? (Amai zo in sync :^)) )",
+            "wie is het meeste dramatisch",
+            "wie is grappiger",
+            "wie is koppiger",
+            "wie houd beter een geheim",
+            "betere danser",
+            "wie verliest rapper hun sleutels",
+            "wie heeft het meeste bang van spinnen/insecten",
+            "wie is het meest/minst organized",
+            "wie is romantischer",
+            "wie is spontaaner",
+            "wie is de betere chef",
+            "wie is de betere datenight planner",
+            "wie zou het langste overleven op een alleen eiland",
+            "wie rijd er beter",
+            "wie is de mooiste",
+            "wie loopt het raps verloren",
+            "wie is liever buiten",
+            "wie zou een vliegtuig crash kunnen redden als piloot (met hulp van AC)",
+        ]
+    }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,88 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '@picocss/pico';
+	import { onMount } from 'svelte';
+	export let data;
+    let questions = [];
+    
+	let index = 0;
+
+	let both = [];
+	let diff = [];
+
+	let maxQuestions = 50;
+
+	function diffPressed() {
+		let q = questions[index];
+		diff.push(q);
+		index++;
+	}
+	function bothPressed() {
+		let q = questions[index];
+		both.push(q);
+		index++;
+	}
+
+	$: count = Math.min(questions.length, maxQuestions);
+
+	onMount(() => {
+		questions = data.questions
+            .sort((a, b) => 0.5 - Math.random())
+            .map(x=> x[0].toUpperCase() + x.slice(1));
+		console.log('shuffle shuffle');
+	});
+</script>
+
+<header>
+	<h1 class="center">Wedding Game</h1>
+</header>
+<main class="container">
+
+	{#if index < count}
+		<article>
+			<h3>Question {index + 1}</h3>
+			<p>
+				{questions[index]}
+			</p>
+			<br />
+			<div class="grid">
+				<button class="outline contrast" on:click={bothPressed}>Both</button>
+				<button class="outline secondary" on:click={() => index++}>SKIP</button>
+				<button class="outline" on:click={diffPressed}>Different</button>
+			</div>
+		</article>
+	{:else}
+		<div class="grid">
+			<details open>
+				<summary>Both</summary>
+                
+				{#each both as b}
+					<p>{b}</p>
+				{/each}
+			</details>
+			<details open>
+				<summary>Different</summary>
+				{#each diff as d}
+					<p>{d}</p>
+				{/each}
+			</details>
+		</div>
+        <button on:click={() => location.reload()}>Reset!</button>
+	{/if}
+
+    <div>
+        <i>Progress:</i>
+        <progress value={index} max={count} />
+    </div>
+    <details>
+        <summary>Settings ⚙️</summary>
+        <label for="range">Questions - {count} 
+            <input type="range" min="1" max="{questions.length}" value="{count}" id="range" name="range" on:input={(e) => maxQuestions=e.target.value}>
+        </label>
+    </details>
+</main>
+
+<style>
+	.center {
+		text-align: center;
+	}
+</style>
